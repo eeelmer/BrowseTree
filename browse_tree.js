af48f707b617fb75e84665b3b1422ffe9e31674f
Elmer Ward <elmer.ward@gmail.com>
2013-04-21 22:53:45 -0700

    Reorganize code. Modify visuals. Add click-to-display-title to visual nodes

--- browse_tree.js
+++ browse_tree.js
@@ -1,95 +1,43 @@
 // --------------------------------------------------------
-// SETUP
+// Handlers
 
-$(window).ready(updateHeight);
-$(window).resize(updateHeight);
-
+$(window).ready(function() {
+  updateHeights();
+  recalculateAndRender();
+});
+$(window).resize(function() {
+  updateHeights();
+  recalculateAndRender();
+});
 document.addEventListener('DOMContentLoaded', function() {
-  initialize();
+  setup();
 });
 
-function updateHeight()
-{
-  // update the height of the svg container div
-  var windowHeight = $(window).height() - 96;
-  // var div = $('#tree');
-  // div.css('height', windowHeight);
-  $('#tree').attr('height', windowHeight);
-
-  // update the height of the root svg element
-  var adjustedHeight = $(window).height() - 96;
-  $("#tree svg").attr("height", adjustedHeight);
-
-  // update the size attribute of the tree for accurate layout
-  //    recalculations
-  tree.size([adjustedHeight, width]);
-
-  renderTree();
-}
-
-function updateWidth(maxDepth) {
-  $("#tree svg").attr("width", 180 * maxDepth + 100);
-}
-
-
-//
-// Setup event listener
-//
-chrome.tabs.onUpdated.addListener(function(tabId, updates, tab) {
-
-  // Is the node for this tab already in the tree?
-  currentNode = document.getElementById(tab.id);
-
-  if(updates.status == 'complete' &&
-     tab.title != 'New Tab' && tab.title != 'Browse Tree' &&
-     currentNode == null) {
-
-    // TESTING
-    console.log('TAB: ' + tab.id + ' OID: ' + tab.openerTabId);
-
-    // var parentIdForTree = 0; // TREE CODE
-
-    var attachNode = document.getElementById('log');
-    var openerTabNode = document.getElementById(tab.openerTabId);
-
-    if(openerTabNode != null) {
-      attachNode = openerTabNode;
-      // parentIdForTree = openerTabNode.id; // TREE CODE
-    } else {
-      // Parent Node was never created. Create it.
-      // Occurs due to inconsistent Chrome API behavior.
-    }
-
-    // Create Node for the new tab and attach it to the attach Node.
-    var newTabNode = document.createElement('li');
-    newTabNode.id = tab.id;
-    newTabNode.innerText = tab.title;
-    newTabNode.appendChild(document.createElement('ul'));
-
-    attachNode.getElementsByTagName('ul')[0].appendChild(newTabNode);
-
-    // addNode(parentIdForTree, newTabNode.id); // TREE CODE
-    addNode(attachNode.id, newTabNode.id, tab.title); // TREE CODE
-  }
+chrome.tabs.onCreated.addListener(function(tab) {
+  // console.log('CREATE' + tab.index);
+  // console.log('TAB: ' + tab.id + ' OID: ' + tab.openerTabId);
+  // console.log('URL: ' + tab.url);
+  // console.log('TITLE: ' + tab.title);
 });
 
 // --------------------------------------------------------
-// TREE Initialization
+// State and Setup
 //
-function initialize() {
+function setup() {
   width = 960;
   height = 600;
 
   tree = d3.layout.tree()
-    .size([height - 20, width - 20]);
+    .size([$(window).height() - 96, width]);
 
-  // Initial node data and layout
+
+  // Set up initial node data and layout
   root = {};
   nodeData = tree.nodes(root);
-
   root.parent = root;
   root.previousX = root.y;
   root.previousY = root.x;
+  root.title = "root";
 
   diagonal = d3.svg.diagonal()
       .projection(function(d)
@@ -101,22 +49,96 @@ function initialize() {
     .attr("width", width)
     .attr("height", height)
     .append("g")
-    .attr("transform", "translate(10,10)");
-
-  // Selection
-  nodes = svg.selectAll(".node"),
-  links = svg.selectAll(".link");
+    .attr("transform", "translate(10, 0)");
 
   duration = 750;
+
+  recalculateAndRender();
 }
+
+
+// --------------------------------------------------------
+// Modifiers
+//
+function updateHeights()
+{
+  // update the height of the svg container div
+  var windowHeight = $(window).height() - 96;
+  $('#tree').attr('height', windowHeight);
+
+  // update the height of the root svg element
+  var adjustedHeight = $(window).height() - 96;
+  $("#tree svg").attr("height", adjustedHeight);
+
+  // update the size attribute of the tree for accurate layout
+  //    recalculations
+  tree.size([adjustedHeight, width]);
+}
+
+function updateWidth(maxDepth) {
+  $("#tree svg").attr("width", maxDepth * 200 + 200);
+}
+
+function toggleDisplay(infoWindow) {
+  currentOpacity = infoWindow.attr("opacity")
+  if(currentOpacity == 0)
+    infoWindow.attr("opacity", 1.0);
+  else
+    infoWindow.attr("opacity", 0);
+}
+
+
+//
+// Setup event listener
+//
+chrome.tabs.onUpdated.addListener(function(tabId, updates, tab) {
+
+  // console.log('UPDATE: ' + updates.status)
+  // console.log('TAB: ' + tab.id + ' OID: ' + tab.openerTabId);
+  // console.log('URL: ' + tab.url);
+  // console.log('TITLE: ' + tab.title);
+
+  if(updates.status == 'complete') {
+
+    // Is the node for this tab already in the tree?
+    var currentNode = document.getElementById(tab.id);
+
+    if(currentNode == null) {
+
+      addNode(tab.openerTabId, tab.id, tab.title); // TREE CODE
+
+      var attachNode = document.getElementById('log');
+      var openerTabNode = document.getElementById(tab.openerTabId);
+
+      if(openerTabNode != null) {
+        attachNode = openerTabNode;
+      } else {
+        // Parent Node was never created. Create it.
+        // Occurs due to inconsistent Chrome API behavior.
+      }
+
+      // Create Node for the new tab and attach it to the attach Node.
+      var newTabNode = document.createElement('li');
+      newTabNode.id = tab.id;
+      var titleSpan = document.createElement('span')
+      titleSpan.innerText = tab.title
+      newTabNode.appendChild(titleSpan)
+      newTabNode.appendChild(document.createElement('ul'));
+
+      attachNode.getElementsByTagName('ul')[0].appendChild(newTabNode);
+    } else {
+      updateNode(tab.id, tab.title) // TREE CODE
+
+      currentNode.getElementsByTagName('span')[0].innerText = tab.title;
+    }
+  }
+});
+
 
 // --------------------------------------------------------
 // TREE Node Addition
 //
 function addNode(parentId, newId, newTitle) {
-
-  // TESTING
-  tree.size([$(window).height() - 96, width - 20]);;
 
   // Create a new node
   var newNode = {id: newId,
@@ -138,11 +160,28 @@ function addNode(parentId, newId, newTitle) {
   if(parentNode.children) parentNode.children.push(newNode); else parentNode.children = [newNode];
   nodeData.push(newNode);
 
-  renderTree();
+  recalculateAndRender();
 }
 
-// update
-function renderTree() {
+// --------------------------------------------------------
+// TREE Node Update
+//
+
+function updateNode(id, title) {
+  for(var i = 0; i < nodeData.length; i++) {
+    if(nodeData[i].id == id) {
+      nodeData[i].title = title;
+      break;
+    }
+  }
+  recalculateAndRender();
+}
+
+
+// --------------------------------------------------------
+// TREE Calculations and Rendering
+//
+function recalculateAndRender() {
   // Calculate Layout
   nodeData = tree.nodes(root);
   linkData = tree.links(nodeData);
@@ -152,16 +191,24 @@ function renderTree() {
   nodeData.forEach(function(d) {
     if (d.depth > maxDepth)
       maxDepth = d.depth;
-    d.y = d.depth * 180;
+    d.y = d.depth * 200;
   });
   updateWidth(maxDepth);
+
+  // Selection
+  nodes = svg.selectAll(".node"),
+  links = svg.selectAll(".link");
 
   // Compute data join.
   nodes = nodes.data(nodeData, function(d) { return d.id; });
   links = links.data(linkData, function(d) { return d.source.id + "-" + d.target.id; });
 
-  // Create and add nodes to hold and display data
-  var nodeGroup = nodes.enter().append("svg:g")
+  // Perform any needed updates
+  nodes.select("text")
+      .text(function(d) { return d.title; });
+
+  // Perform any needed additions
+  var nodeGroup = nodes.enter().append("g")
       .attr("class", "node")
       .attr("transform", function(d) {
         return "translate(" + d.parent.previousX + "," + d.parent.previousY + ")";
@@ -169,7 +216,16 @@ function renderTree() {
 
   nodeGroup.append("circle")
       .attr("fill", "whitesmoke")
-      .attr("r", 3);
+      .attr("r", 4)
+      .on("click", function() {
+        toggleDisplay($(this).next());
+      });
+
+  nodeGroup.append("text")
+      .attr("stroke", "none")
+      .attr("y", 20)
+      .text(function(d) { return d.title; })
+      .attr("opacity", 0);
 
   // Add entering links in the parent’s old position.
   links.enter().insert("path", ".node")
